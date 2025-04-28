3d008514877555c9645567599aa8f6ee03c1970b
aaelim <iamgonoooo@gmail.com>
2025-04-28 20:29:08 +0700

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-# src/model.py
-import torch
-from torch import nn
-from torchvision import models
-
-def get_model():
-    model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
-    model.fc = nn.Linear(model.fc.in_features, 1)
-    return model
